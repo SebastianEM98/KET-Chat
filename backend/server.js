f065ffcab3333e535d6cb6dbcc14a1be7067dd38
Sebastian Echeverry <sebas.eche98@gmail.com>
2024-10-08 14:30:57 -0500

    CORS setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -15,6 +16,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// CORS Setup
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
